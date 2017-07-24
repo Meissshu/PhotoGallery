02838a1e1747773c53729f96645e49141784bf1e
Meissshu <allayarov.marsel4305@mail.ru>
2017-07-24 17:47:17 +0300

    Queue data
    Update queueThumbnail to use message, handler, looper system to download bitmaps when it is needed.

--- app/src/main/java/com/meishu/android/photogallery/dataUtils/ThumbnailDownloader.java
+++ app/src/main/java/com/meishu/android/photogallery/dataUtils/ThumbnailDownloader.java
@@ -1,7 +1,16 @@
 package com.meishu.android.photogallery.dataUtils;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by Meishu on 24.07.2017.
@@ -10,6 +19,14 @@ import android.util.Log;
 public class ThumbnailDownloader<T> extends HandlerThread {
 
     public static final String TAG = "ThumbnailDownloader";
+    public static final int MESSAGE_DOWNLOAD = 0;
+
+    private Handler requestHandler;
+    private ConcurrentMap<T, String> requestMap = new ConcurrentHashMap<>();
+
+    public ThumbnailDownloader(Handler responseHandler) {
+        super(TAG);
+    }
 
     public ThumbnailDownloader() {
         super(TAG);
@@ -17,7 +34,42 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "Got url: " + url);
+
+        if (url == null) {
+            requestMap.remove(target);
+        } else {
+            requestMap.put(target, url);
+            requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+                    .sendToTarget();
+        }
     }
 
+    @Override
+    protected void onLooperPrepared() {
+        requestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a REQUEST for url: " + requestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
+    }
 
+    private void handleRequest(final T target) {
+        try {
+            final String url = requestMap.get(target);
+            if (url == null)
+                return;
+
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+        }
+         catch (IOException e) {
+           Log.e(TAG, "Error downloading image", e);
+        }
+    }
 }
